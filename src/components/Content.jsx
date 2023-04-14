97ebec5bb98feb37026ccadf8eebe0fa37a5993f
YOUR NAME <YOUR@EMAIL.com>
2023-04-13 18:04:18 -0700

    saving initial setup

---
+++ src/components/Content.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Content() {
+    const myClass = "text-green"
+  return (
+    <h1 className={`text-3xl font-bold underline ${myClass}`}>MyLogo</h1>
+  )
+}
+
+export default Content
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [ "./index.html",
+  "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      'green': 'hsl(140, 100%, 23%)'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
